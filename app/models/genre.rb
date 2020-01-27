95f8ba7a215c13ac1f3faf2752ecaf3af317396c
Jennifer Miller <jmillerarcher@me.com>
2020-01-27 16:34:32 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,7 +3,9 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    
+       self.songs.count
+
   end
 
   def artist_count
