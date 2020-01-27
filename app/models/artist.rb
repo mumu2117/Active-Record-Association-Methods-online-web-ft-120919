90a47e8109f48f7f72383e192836e7fdee185fbf
Jennifer Miller <jmillerarcher@me.com>
2020-01-27 16:34:11 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    
     self.songs.first.genre
   end
 
@@ -14,7 +14,7 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+  
         self.genres.count
 
   end
